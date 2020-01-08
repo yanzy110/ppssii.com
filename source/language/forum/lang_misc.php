2f7d597ac8c73a32ca77e36910c366791a4ef7e8
cscsb <49179157+cscsb@users.noreply.github.com>
2020-01-08 12:32:55 +0800

    3

--- source/language/forum/lang_misc.php
+++ source/language/forum/lang_misc.php
@@ -325,8 +325,8 @@ $lang = array
 	'plugin_memo' => '您有 <span class="xi1">{number}</span> 款应用有可用更新',
 	'plugin_link' => '现在更新',
 
-	'seccode' => '验证码',
-	'seccode_update' => '换一个',
+	'seccode' => 'verification code',
+	'seccode_update' => 'change a verification code',
 	'seccode_player' => '<span style="padding:2px"><img border="0" style="vertical-align:middle" src="static/image/common/seccodeplayer.gif" /> <a href="javascript:;" onclick="window.document.seccodeplayer_{idhash}.SetVariable(\'isPlay\', 1)">播放验证码</a></span>',
 	'secqaa' => '验证问答',
 
